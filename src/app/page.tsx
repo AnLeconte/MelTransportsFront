0b44c3085d9c51f122596c557363b698247ca37a
Anna RAACH <annagraph92@gmail.com>
2024-08-29 12:19:56 +0200

    feat: move footer and header on layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Header from "./header/Header";
-import Footer from "./footer/Footer";
 import SearchItineraireForm from "./searchItineraireForm/SearchItineraireForm";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <div className="flex-grow relative items-center">
         <img
           src="https://s3-alpha-sig.figma.com/img/45e1/635a/f6ef3ec08c19da482c85c94f437e960a?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=amfVXvxli8PWMseYuFcYMCvWi77djsLkta5pbT3UA6ju9gTOgOONRZjNp2dzXqCxYpV2v-h~-QWAl5BNuXGhGcDqCmXV17kILMdaRpiuDRWdHXpSXYdXOeaqjXn3crkhXy6jKQ3ywCDiBOxKtwva2ZAOijtLhNhAmu-AQiMQY0-W3BjAjd6exw3iqg9lsSKjxiXuaITJ7epCcVwE58P8v311mEo4satZN4jFZwaEkWbPBc2NTubX4hmS5Jh7i8R8tASqsuZ-pglAoHF7CEATbHY2IvjiZ5~03lgxwAKvBdPkulGZ1-VvJi00kZlnCwQsd-Y3mJqA88zCHM4TQi8GMw__"
@@ -17,7 +14,6 @@ export default function Home() {
           <SearchItineraireForm />
         </div>
       </div>
-      <Footer />
     </div>
   );
 }
