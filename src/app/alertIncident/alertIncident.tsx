dc584e4b83fe1f496f7aba588168ae51ab9caa41
Juliette117 <ajuliette7@gmail.com>
2024-08-29 21:37:13 +0200

    fix: alerte incident

--- src/app/alertIncident/alertIncident.tsx
+++ src/app/alertIncident/alertIncident.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import WarningBig from "@/asset/WarningBig";
 
 const Alert = () => {
-    return (
-        <div className="w-full h-[64px] bg-alert flex items-center  px-4">
-           <div className=" inline-flex font-medium text-[17px] text-incident ml-7">
-        <p className="inline-flex"><WarningBig /> <span className="ml-3 mb-1.5 flex content-end">Travaux sur la ligne [ligne] : l'arrêt [station] n'est pas desservi</span></p>
-      </div>
+  return (
+    <div className="w-full h-[64px] bg-alert flex items-center  px-4">
+      <div className=" inline-flex font-medium  ml-7"></div>
+      <WarningBig /><p className="text-[17px] text-incident ml-3 mb-1.5 flex content-end">Travaux sur la ligne [ligne] : l'arrêt [station] n'est pas desservi</p>
+    </div>
 
 
-        </div>
-    );
+
+  );
 };
 
 export default Alert;
