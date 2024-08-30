82279f06b7090be91c4a83e67f945410f5f04179
Anna RAACH <annagraph92@gmail.com>
2024-08-30 13:56:38 +0200

    style: styling perturbation page

--- src/app/perturbations/page.tsx
+++ src/app/perturbations/page.tsx
@@ -1,25 +1,36 @@
 "use client";
 
 import React from "react";
-import { useIncidentData } from "../hook/useIncidentData"
-
-
+import { useIncidentData } from "../hook/useIncidentData";
 
 const AlertIncidentPage = () => {
-  const { dataIncident } = useIncidentData() 
-
-
+  const { dataIncident } = useIncidentData();
 
   return (
-    <div className="min-h-screen p-4">
-      <h1 className="text-center font-extrabold text-red-600 text-2xl">Infos Perturbations</h1>
-      {dataIncident.map((incident) => (
-        <ul key={incident.id} className="mb-4 border p-2 rounded text-center  text-red-600">
-          <li className="font-bold">{incident.intitule}</li>
-          <li>{incident.details}</li>
-          <li>Les station: {incident.stations} sont impactés veuillez nous excuser pour ce désagrément </li>
-        </ul>
-      ))}
+    <div className="min-h-screen flex justify-center items-start p-4 mt-10">
+      <div className="text-center">
+        <h1 className="font-extrabold text-red-600 text-2xl uppercase mb-4">
+          Infos Perturbations
+        </h1>
+        <div className="w-[500px] h-[250px] flex items-center justify-center bg-whiteIsh shadow-md">
+          <div>
+            {dataIncident.map((incident) => (
+              <ul
+                key={incident.id}
+                className="mb-4 p-2 rounded text-center text-red-600"
+              >
+                <li className="font-bold underline">{incident.intitule}</li>
+                <br />
+                <li>{incident.details}</li>
+                <li>
+                  Les stations : {incident.stations} sont impactées. Veuillez
+                  nous excuser pour ce désagrément
+                </li>
+              </ul>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
