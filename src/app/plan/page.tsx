e23f80e599d01255e6bb80221e4fbb922e032a7c
Juliette117 <ajuliette7@gmail.com>
2024-08-30 13:51:06 +0200

    feat: page plan (y a r)

---
+++ src/app/plan/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
+const Plan = () => {
+
+    return (
+        <div className="h-screen">
+            <h1 className="my-10 text-center text-4xl">Plan du réseau</h1>
+
+        </div>
+    );
+};
+
+export default Plan;
