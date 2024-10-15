a5a07847b85971c5f6f73de11aaf4e5449584b5f
Anna RAACH <annagraph92@gmail.com>
2024-10-15 15:05:36 +0200

    feat: add background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@
 import axios from "axios";
 import SearchItineraireForm from "./searchItineraireForm/SearchItineraireForm";
 import IncidentsAlert from "./incidentsAlert/IncidentsAlert";
-import SearchDiv from "./searchDiv/SearchDiv";
-import Alert from "./alertIncident/alertIncident";
 import {useEffect, useState} from "react";
 
 export default function Home() {
@@ -36,7 +34,7 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <div className="flex-grow relative items-center">
         <img
-          src="https://s3-alpha-sig.figma.com/img/45e1/635a/f6ef3ec08c19da482c85c94f437e960a?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=amfVXvxli8PWMseYuFcYMCvWi77djsLkta5pbT3UA6ju9gTOgOONRZjNp2dzXqCxYpV2v-h~-QWAl5BNuXGhGcDqCmXV17kILMdaRpiuDRWdHXpSXYdXOeaqjXn3crkhXy6jKQ3ywCDiBOxKtwva2ZAOijtLhNhAmu-AQiMQY0-W3BjAjd6exw3iqg9lsSKjxiXuaITJ7epCcVwE58P8v311mEo4satZN4jFZwaEkWbPBc2NTubX4hmS5Jh7i8R8tASqsuZ-pglAoHF7CEATbHY2IvjiZ5~03lgxwAKvBdPkulGZ1-VvJi00kZlnCwQsd-Y3mJqA88zCHM4TQi8GMw__"
+          src="/assets/backgroundImage.png"
           alt="Background"
           className="absolute inset-0 w-full h-full object-cover object-center opacity-50"
           style={{ objectPosition: "center 10%" }}
